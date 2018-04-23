e31a01e6396f1594e5456ce16c07819ef3a766e9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DXTreeList - How to implement summary footers
+
+
+<p>At the moment, our TreeListView does not provide the capability to show <a href="http://documentation.devexpress.com/#WindowsForms/CustomDocument1070"><u>summary footers</u></a> for its nodes. This example demonstrates how to implement this functionality manually by using a custom <strong>DataRowTemplate</strong>.<br /><br />This workaround has a few limitations:<br /> 1. It supports only a two-level structure.<br />2. The context menu for summary footers is not supported.<br />3. The grid's horizontal scrolling is not supported.</p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Windows
+Imports System.Collections.ObjectModel
+Imports DevExpress.Xpf.Grid
+
+Namespace DXTreeListSample
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Private Index As Integer = 10
+		Public Property Items() As ObservableCollection(Of SomeObject)
+		Public Sub New()
+			Items = New ObservableCollection(Of SomeObject)()
+			For i As Integer = 0 To 9
+				Items.Add(New SomeObject(String.Format("First level - {0}", i), i, i))
+				For j As Integer = 0 To 9
+                    Items.Add(New SomeObject(String.Format("First level - {0}.{1}", i, j), i, Index))
+                    Index += 1
+				Next j
+			Next i
+			InitializeComponent()
+			grid.ItemsSource = Items
+		End Sub
+	End Class
+	Public Class SomeObject
+		Public Sub New()
+		End Sub
+		Public Sub New(ByVal name As String, ByVal parentId As Integer, ByVal id As Integer)
+			Me.Id = id
+			Me.ParentId = parentId
+			Me.Name = name
+		End Sub
+		Public Property Id() As Integer
+		Public Property ParentId() As Integer
+		Public Property Name() As String
+	End Class
+End Namespace
